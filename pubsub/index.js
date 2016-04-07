d3f38ac9173b8d63c5f0a5288e932e14da561da3
Jason Polites <jason.polites@gmail.com>
2016-04-07 14:13:02 -0700

    Initial commit

---
+++ pubsub/index.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+  // Create a pubsub client
+  var pubsub = gcloud.pubsub({
+    // We're using the API from the same project as the Cloud Function
+    projectId: process.env.GCP_PROJECT,
+  });  
+
+  /**
+   * Publishes a message to a Cloud Pub/Sub Topic 
+   */
+  "publish" : function (context, data) {
+    var topicName = data['topic'];
+    var message = data['message'];
+
+    if(!topicName) {
+      context.failure("Topic not provided.  Make sure you have a 'topic' property in your request");
+    } else {
+      if(!message) {
+        context.failure("Message not provided.  Make sure you have a 'message' property in your request");
+      } else {
+        console.log('Publishing message to topic ' + topicName);
+
+        // The Pub/Sub topic must already exist
+        var topic = pubsub.topic(topicName);
+        topic.publish({
+          data: message
+        }, function(err) {
+          if(err) {
+            context.failure(err);
+          } else {
+            context.success();
+          }
+        });        
+      }
+    }
+  },
+
+  /**
+   * Triggered from a message on a Pub/Sub topic 
+   */
+  "subscribe" : function (context, data) {
+    // We're just going to log the message to prove that it worked!
+    console.log(data['message']);
+
+    // Don't forget to call success!
+    context.success();
+  }
+}
